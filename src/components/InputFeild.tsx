1d90567ed6c9beb12587fca73e9d0a5ec6198f2b
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-01 21:09:26 +0300

    Input feild component added

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './styles.css';
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:(e:React.FormEvent)=> void;
+}
+const InputFeild:React.FC<Props> = ({ todo, setTodo , handleAdd}: Props) =>{
+    return (
 
-function InputFeild() {
-  return (
-    <div>InputFeild</div>
-  )
+        <form className='input' onSubmit={handleAdd}>
+            <input type="input" value={todo} onChange={(e)=>setTodo(e.target.value)} placeholder='Enter a task' className='input_box' />
+            <button className='input_submit' type='submit'>Go</button>
+        </form>
+
+    )
 }
 
 export default InputFeild
