ec2bb80adb8dd6a668aec52d5e0cff9d42726de0
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-01 21:10:08 +0300

    App edited

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from './model';
 
 function App() {
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e:React.FormEvent<Element>)=>{
+
+     e.preventDefault();
+     if (todo) {
+      setTodos([...todos, {id:Date.now(), todo:todo,isDone:false}]) 
+      setTodo("");
+     }
+     
+  }
   return (
     <div className="App">
       <span className="heading">TaskList</span>
-      <InputFeild/>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      {/* <TodoList/> */}
     </div>
   );
 }
