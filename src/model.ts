ac7d35a181010228f46f79dfb91cccd7a8161512
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-02 13:10:42 +0300

    Todo model

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,39 @@
-export interface Todo{
-    id:number;
-    todo:string;
-    isDone:boolean;
+// import { Todo } from './model';
+export interface Todo {
+    id: number;
+    todo: string;
+    isDone: boolean;
 }
+// type Actions =
+//     | { type: "add", paload: string }
+//     | { type: "remove", paload: number }
+//     | { type: "done", paload: number }
+
+// const TodoReducer = (state: Todo[], action: Actions) => {
+//     switch (action.type) {
+//         case "add":
+//             return [...state, { id: Date.now(), todo: action.paload, isDone: false }]
+//             break;
+//         case "remove":
+//             return [state.filter((todo) => todo.id !== action.paload)]
+//             break;
+//         case "done":
+//             return state.map((todo)=>
+//             todo.id !== action.paload ? {...todo,isDone:!todo.isDone}:todo
+//             )
+//             break;
+//         default:
+//             return state;
+//             break;
+//     }
+// }
+
+
+// import { useReducer } from "react";
+// const ReducerExample = () => {
+//     const [state, dispatch] = useReducer(TodoReducer, [])
+
+//     return (
+//         <div></div>
+//     )
+// }
